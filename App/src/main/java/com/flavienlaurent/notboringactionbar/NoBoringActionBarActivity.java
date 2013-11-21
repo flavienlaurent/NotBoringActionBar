8685a28e9eed1a6f866a8eebaba87d5a9c28135c
flavienlaurent <flavien.laurent@gmail.com>
2013-11-21 14:12:11 +0100

    optimization reuse Rect instances

--- App/src/main/java/com/flavienlaurent/notboringactionbar/NoBoringActionBarActivity.java
+++ App/src/main/java/com/flavienlaurent/notboringactionbar/NoBoringActionBarActivity.java
@@ -3,24 +3,18 @@ package com.flavienlaurent.notboringactionbar;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.content.res.Resources;
-import android.graphics.RectF;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NoBoringActionBarActivity extends Activity {
 
@@ -34,6 +28,9 @@ public class NoBoringActionBarActivity extends Activity {
     private View mHeader;
     private View mPlaceHolderView;
     private AccelerateDecelerateInterpolator mSmoothInterpolator;
+
+    private Rect mRect1 = new Rect();
+    private Rect mRect2 = new Rect();
 
     public static float clamp(float f1, float f2, float f3) {
         return Math.max(Math.min(f1, f3), f2);
@@ -86,16 +83,13 @@ public class NoBoringActionBarActivity extends Activity {
     }
 
     private void interpolate(View view1, View view2, float interpolation) {
-        RectF rectF1 = getOnScreenRect(view1);
-        RectF rectF2 = getOnScreenRect(view2);
+        Rect rect1 = getOnScreenRect(mRect1, view1);
+        Rect rect2 = getOnScreenRect(mRect2, view2);
 
-        Log.d(TAG, "rectF1: " + rectF1);
-        Log.d(TAG, "rectF2: " + rectF2);
-
-        float scaleX = 1.0F + interpolation * (rectF2.width() / rectF1.width() - 1.0F);
-        float scaleY = 1.0F + interpolation * (rectF2.height() / rectF1.height() - 1.0F);
-        float translationX = 0.5F * (interpolation * (rectF2.left + rectF2.right - rectF1.left - rectF1.right));
-        float translationY = 0.5F * (interpolation * (rectF2.top + rectF2.bottom - rectF1.top - rectF1.bottom));
+        float scaleX = 1.0F + interpolation * (rect2.width() / rect1.width() - 1.0F);
+        float scaleY = 1.0F + interpolation * (rect2.height() / rect1.height() - 1.0F);
+        float translationX = 0.5F * (interpolation * (rect2.left + rect2.right - rect1.left - rect1.right));
+        float translationY = 0.5F * (interpolation * (rect2.top + rect2.bottom - rect1.top - rect1.bottom));
 
         view1.setTranslationX(translationX);
         view1.setTranslationY(translationY - mHeader.getTranslationY());
@@ -103,13 +97,9 @@ public class NoBoringActionBarActivity extends Activity {
         view1.setScaleY(scaleY);
     }
 
-    private RectF getOnScreenRect(View view) {
-        RectF rectF = new RectF(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
-        for (ViewParent parent = view.getParent(); (parent != null) && ((parent instanceof ViewGroup)); parent = parent.getParent()) {
-            ViewGroup viewGroup = (ViewGroup) parent;
-            //rectF.offset(viewGroup.getLeft(), viewGroup.getTop());
-        }
-        return rectF;
+    private Rect getOnScreenRect(Rect rect, View view) {
+        rect.set(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+        return rect;
     }
 
     public int getScrollY() {
