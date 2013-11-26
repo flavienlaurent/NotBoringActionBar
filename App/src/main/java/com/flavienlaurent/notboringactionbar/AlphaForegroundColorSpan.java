bba23b113c27b88a9eb86a095bc5182e9f4b55ea
flavienlaurent <flavien.laurent@gmail.com>
2013-11-26 16:59:52 +0100

    AlphaForegroundColorSpan improvement

--- App/src/main/java/com/flavienlaurent/notboringactionbar/AlphaForegroundColorSpan.java
+++ App/src/main/java/com/flavienlaurent/notboringactionbar/AlphaForegroundColorSpan.java
@@ -2,47 +2,37 @@ package com.flavienlaurent.notboringactionbar;
 
 import android.graphics.Color;
 import android.os.Parcel;
-import android.text.ParcelableSpan;
 import android.text.TextPaint;
-import android.text.style.CharacterStyle;
-import android.text.style.UpdateAppearance;
+import android.text.style.ForegroundColorSpan;
 
-public class AlphaForegroundColorSpan extends CharacterStyle implements UpdateAppearance, ParcelableSpan {
+public class AlphaForegroundColorSpan extends ForegroundColorSpan {
 
-    public static final int FOREGROUND_COLOR_SPAN = 2;
+    private float mAlpha;
 
-    private int mColor;
-
-	public AlphaForegroundColorSpan(int color) {
-        mColor = color;
+    public AlphaForegroundColorSpan(int color) {
+        super(color);
 	}
 
     public AlphaForegroundColorSpan(Parcel src) {
-        mColor = src.readInt();
-    }
-    
-    public int getSpanTypeId() {
-        return FOREGROUND_COLOR_SPAN;
-    }
-    
-    public int describeContents() {
-        return 0;
+        super(src);
+        mAlpha = src.readFloat();
     }
 
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(mColor);
+        dest.writeFloat(mAlpha);
     }
-
-	public int getForegroundColor() {
-		return mColor;
-	}
 
 	@Override
 	public void updateDrawState(TextPaint ds) {
-		ds.setColor(mColor);
+		ds.setColor(getAlphaColor());
 	}
 
     public void setAlpha(float alpha) {
-        mColor = Color.argb((int) (alpha * 255), Color.red(mColor), Color.green(mColor), Color.blue(mColor));
+        mAlpha = alpha;
+    }
+
+    private int getAlphaColor() {
+        int foregroundColor = getForegroundColor();
+        return Color.argb((int) (mAlpha * 255), Color.red(foregroundColor), Color.green(foregroundColor), Color.blue(foregroundColor));
     }
 }
